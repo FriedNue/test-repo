90d398253b524ab9e279074483ac2ef42c194079
DESKTOP-4QIN669\Administrator <DESKTOP-4QIN669\Administrator>
2023-12-07 20:15:38 +0900

    Add [Label1,2], [PictureBox1], [Button1,2]

--- Tutorial/Tutorial/Form1.Designer.cs
+++ Tutorial/Tutorial/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Base 05", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(850, 55);
+            label1.TabIndex = 0;
+            label1.Text = "Test Text";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 53);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(780, 336);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Base 05", 24F, FontStyle.Bold);
+            button1.Location = new Point(12, 397);
+            button1.Name = "button1";
+            button1.Size = new Size(71, 43);
+            button1.TabIndex = 2;
+            button1.Text = "<";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Base 05", 24F, FontStyle.Bold);
+            button2.Location = new Point(721, 397);
+            button2.Name = "button2";
+            button2.Size = new Size(71, 43);
+            button2.TabIndex = 3;
+            button2.Text = ">";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Base 05", 24F, FontStyle.Bold);
+            label2.Location = new Point(370, 400);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 40);
+            label2.TabIndex = 4;
+            label2.Text = "1/8";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 451);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
     }
 }
--- Tutorial/Tutorial/obj/Debug/net8.0-windows/Tutorial.AssemblyInfo.cs
+++ Tutorial/Tutorial/obj/Debug/net8.0-windows/Tutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf1fca9dd774779f362e6c981ef2746b76e7c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
